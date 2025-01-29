75ac991024bc61bbab8f91b544e94e83d2268d56
a6hi <saraffabhishek@outlook.com>
2025-01-29 01:07:34 -0500

    initial project setup

---
+++ src/main/java/com/example/satellite_launch_risk_assessment/SatelliteLaunchRiskAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.satellite_launch_risk_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatelliteLaunchRiskAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatelliteLaunchRiskAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/satellite_launch_risk_assessment/SatelliteLaunchRiskAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.satellite_launch_risk_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatelliteLaunchRiskAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
